9e7efdf8d25ba2118f9c57b6d7011d8738a3960d
Sten Sjo:berg <stenogsjoberg@gmail.com>
2021-05-04 14:02:55 -0400

    added logging

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,10 @@
 export const debug = {
     flashlight: false,
     yMovement: false,
+    logging: true,
+}
+
+export const startMenu = {
+    titleText: "Mazeophobia",
+    startButtonText: "Start",
 }
